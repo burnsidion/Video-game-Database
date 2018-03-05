84dab654fdf3b88abe7df06d35e3d705ed550d06
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-03-05 10:34:57 -0700

    Added new crazy one liner thing, search form

--- js/script.js
+++ js/script.js
@@ -4,74 +4,80 @@ $('document').ready(function() {
   $("#search-button").on("click", function() {
     $("#results").empty();
   })
-  $("#search-button").on("click", function() {
+
+  $("#search-form").on("submit", searchHandler);
+
+  function searchHandler(e) {
+    e.preventDefault();
     var query = $("#search-input").val();
     var url = `https://www.giantbomb.com/api/games/?api_key=38cff389d031a893f76688dcf11e5024bdf0f523&sort=original_release_date:asc`;
 
-    if (query === '') {
-      alert("Please enter a game to search for ya DINGUS!!")
+    $.ajax({
+      url: "http://api.giantbomb.com/search",
+      dataType: "jsonp",
+      jsonp: "json_callback",
+      data: {
+        api_key: '38cff389d031a893f76688dcf11e5024bdf0f523',
+        limit: 100,
+        query: `"${query}"`,
+        format: "jsonp",
+        field_list: "name,image,original_release_date,description,expected_release_year,platforms",
+        resources: "game,platform",
+        results: 100
+      },
 
-    } else {
+      success: function(results) {
+        //console.log(results);
+        console.log('RESULTS: ', results.results);
+        for (var i = 0; i < results.results.length; i++) {
+          let name = results.results[i].name;
+          let image = results.results[i].image.small_url;
+          let expecRel = results.results[i].expected_release_year;
+          let releaseDate = results.results[i].original_release_date;
+          let platformList = results.results[i].platforms;
+          let description = results.results[i].description;
 
-      $.ajax({
-        url: "http://api.giantbomb.com/search",
-        dataType: "jsonp",
-        jsonp: "json_callback",
-        data: {
-          api_key: '38cff389d031a893f76688dcf11e5024bdf0f523',
-          limit: 100,
-          query: `"${query}"`,
-          format: "jsonp",
-          field_list: "name,image,original_release_date,description,expected_release_year,platforms",
-          resources: "game,platform",
-          results: 100
-        },
+          // if(description === null){
+          //   description = "Description could not be found!!"
+          // }
+          // console.log(description);
 
-        success: function(results) {
-          //console.log(results);
-          console.log('RESULTS: ', results.results);
-          for (var i = 0; i < results.results.length; i++) {
-            let name = results.results[i].name;
-            let image = results.results[i].image.small_url;
-            let expecRel = results.results[i].expected_release_year;
-            let releaseDate = results.results[i].original_release_date;
-            let platformList = results.results[i].platforms;
-            let description = results.results[i].description;
-            //console.log(i, description);
+          // let platformsArr = []
+          //
+          // if(platformList === null){
+          //   platformsArr = "UH-OH! No Platforms could be found!!!"
+          // } else {
+          //    platformsArr = platformList.map(function(platform) {
+          //
+          //     return platform.name
+          //   })
+          // }
 
-            let platformsArr = []
 
-            if(platformList === null){
-              platformsArr = "UH-OH! No Platforms could be found!!!"
-            } else {
-               platformsArr = platformList.map(function(platform) {
+          const platformsArr = (platformList === null)
+            ? `No platforms!`
+            : platformList.map(p => p.name);
 
-                return platform.name
-              })
-            }
-
-            // console.log(i, platformList, platformsArr);
-
-            if (releaseDate === null) {
-              releaseDate = "TBA"
-            }
-
-            releaseDate = releaseDate.replace('00:00:00', '')
-
-            //var platforms = results.results[i].api_detail_url[];
-            //console.log(platforms);
-            let game = `<div class="game">
-            <h2> ${name} </h2>
-            <p> Released: ${releaseDate} </p>
-            <p> Platforms: ${platformsArr} </p>
-            <img class="image" src= ${image}>
-            </div>`
-
-            $('#results').append(game);
+          if (releaseDate === null) {
+            releaseDate = "TBA"
           }
-        }
 
-      });
-    }
-  })
+          releaseDate = releaseDate.replace('00:00:00', '')
+
+          //var platforms = results.results[i].api_detail_url[];
+          //console.log(platforms);
+          let game = `<div class="game">
+          <h2> ${name} </h2>
+          <p> Released: ${releaseDate} </p>
+          <p> Platforms: ${platformsArr} </p>
+          <img class="image" src= ${image}>
+          </div>`
+
+          $('#results').append(game);
+        }
+      }
+
+    });
+
+  }
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,6 @@
 ### MVP for Project
 -[] user should be able to enter in name of game or single word.
+
 -[] will be returned a list of all games on all consoles with that name or name containing word in input
+
 -[] if the word cannot be found or returns to many results, return, "There are either too many games or no games, could you be more specific?"
-//TODO IMPORTANT!!! Need to somehow append images to results that are displayed.
- //  TODO Need to add different Images to page
-// TODO Need to have the site refresh when a new search is entered, clearing results from previous Search
