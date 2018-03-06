930371ca85a694403cd5c359a31b037317d537d3
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-03-06 10:07:19 -0700

    added modal for description link on each game title

--- js/script.js
+++ js/script.js
@@ -61,10 +61,30 @@ $('document').ready(function() {
           //MAKE EACH TITLE A CLICKABLE LINK TO THE DESCRIPTION
 
           let game = `<div class="game col-sm-4">
-          <h3> ${name} </h3>
+          <h3> <a href="#modal${i}" data-target="#modal${i}" data-toggle="modal"> ${name} </a> </h3>
           <p> Released: ${releaseDate} </p>
           <p> Platforms: ${platformsArr} </p>
           <img class="image" src= ${image}>
+          </div>`
+
+          let gameDescrip = `<div class="modal fade" id="modal${i}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p class="game-descrip">${description}</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
           </div>`
 
           //
@@ -74,7 +94,12 @@ $('document').ready(function() {
 
 
           $('#results').append(game);
+          $('#body').append(gameDescrip)
         }
+
+         //$('.modal').modal()
+
+
       }
 
     });
